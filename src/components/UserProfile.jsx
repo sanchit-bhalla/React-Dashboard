2e7e3235e537bfeb2ca7ec07142d41e0821a5a90
sanchit-bhalla <sanchitbhalla91@gmail.com>
2022-09-26 12:46:07 +0530

    Admin name Updated

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -32,7 +32,7 @@ const UserProfile = () => {
         <div>
           <p className="font-semibold text-xl dark:text-gray-200">
             {" "}
-            Michael Roberts{" "}
+            Sanchit Bhalla{" "}
           </p>
           <p className="text-gray-500 text-sm dark:text-gray-400">
             {" "}
